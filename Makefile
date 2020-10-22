ba679472c896195d733225805d66d2092201dfb0
Jiri Popelka <jpopelka@redhat.com>
2020-10-22 17:57:05 +0200

    Allow ssh key with a passphrase

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build-onboard:
 	podman build . -t centos-onboard -f onboard/Containerfile
 
 run-onboard:
-	podman run --rm --cap-add=SYS_ADMIN \
+	podman run --rm -ti --cap-add=SYS_ADMIN \
 	-v ${HOME}/.ssh:/my-ssh:ro,Z \
 	-v ${PWD}/onboard/input:/in:rw,Z \
 	-e PAGURE_TOKEN=${PAGURE_TOKEN} \
--- onboard/run-onboard.sh
+++ onboard/run-onboard.sh
@@ -1,9 +1,20 @@
+#!/usr/bin/bash
+
 mkdir --mode=0700 -p "${HOME}/.ssh"
+
 pushd "${HOME}/.ssh"
-install -m 0400 /my-ssh/id_rsa .
-install -m 0400 /my-ssh/id_rsa.pub .
+install -m 0600 /my-ssh/id_rsa .
+install -m 0644 /my-ssh/id_rsa.pub .
+
+eval $(ssh-agent -s)
+ssh-add id_rsa
+ssh-add -l
+
 ssh-keyscan git.stg.centos.org >>known_hosts
 ssh-keyscan gitlab.com >>known_hosts
 popd
+
+# debugging
+# export GIT_SSH_COMMAND="ssh -vvv"
 
 python3 onboard.py
